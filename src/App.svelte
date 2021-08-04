65349ce00fed01239bd9388ee64613527e822d00
izaanjahangir <izaanjahangir2@gmail.com>
2021-08-04 13:52:33 +0500

    installed material ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,36 @@
-<main>
-	<p>Hello world</p>
-</main>
+<script>
+  import Card, { Content } from "@smui/card";
+  import Textfield from "@smui/textfield";
+
+  let todoText = "";
+</script>
+
+<div class="container">
+  <div class="card-container">
+    <Card style="height: 100%">
+      <Content>
+        <Textfield
+          bind:value={todoText}
+          class="shaped-outlined"
+          variant="outlined"
+          label="Label"
+        />
+      </Content>
+    </Card>
+  </div>
+</div>
+
+<style>
+  .container {
+    height: 100%;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+  }
+
+  .card-container {
+    height: 90%;
+    width: 90%;
+  }
+</style>
